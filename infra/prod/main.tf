5f7af3793d2c5f9c5b840c76ed851375153fb14c
Nina CHAUCHARD <nina.chauchard@protonmail.com>
2026-04-22 23:58:15 +0200

    fix AMI filter in prod

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -31,13 +31,6 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-
 # -------- VPC --------
 data "aws_vpc" "default" {
   default = true
